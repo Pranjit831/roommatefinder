2d2f2deb18be11cc42ff586e067416500ba0d62d
Pranjit831 <70503299+Pranjit831@users.noreply.github.com>
2020-09-30 20:08:01 +0530

    Update resp.js

--- js/resp.js
+++ js/resp.js
@@ -8,4 +8,21 @@ burger.addEventListener('click',()=>{
 	rightNav.classList.toggle('v-class-resp');
 	navList.classList.toggle('v-class-resp');
 	navbar.classList.toggle('h-nav-resp');
-})
+});
+myform.addEventListener('click',(e)=>{
+	e.preventDefault();
+	Swal.fire({
+  title: '<strong>Submitted</strong>',
+  icon: 'success',
+  html:
+    'How much you like the project, press the thumbs-up',
+  // showCloseButton: true,
+  showCancelButton: true,
+  focusConfirm: false,
+  confirmButtonText:
+    '<i class="fa fa-thumbs-up"></i> Great!',
+  confirmButtonAriaLabel: 'Thumbs up',
+  cancelButtonText:
+    '<i class="fa fa-thumbs-down"></i> Excellent!',
+  cancelButtonAriaLabel: 'Thumbs down'})
+});
